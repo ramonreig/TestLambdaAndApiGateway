22486f46440bfaf2655668661626a0d5080edd00
ramonreig <80717304+ramonreig@users.noreply.github.com>
2021-03-15 16:06:27 -0400

    Update hellomiamiteam.py

--- Serverless_Webite_Demo/hellomiamiteam.py
+++ Serverless_Webite_Demo/hellomiamiteam.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
         "headers": {
             "Access-Control-Allow-Origin": "*",
         },
-        "body": "Keep being awesome Cloud Gurus!"
+        "body": "Keep being awesome Miami Team!"
     }
     
     return resp
